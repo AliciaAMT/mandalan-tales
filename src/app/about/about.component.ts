84bae6f872a38a4a2e209f509cca61c4517905e4
AliciaAMT <46018401+AliciaAMT@users.noreply.github.com>
2025-06-16 12:57:02 -0500

    scaffolding

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { AppHeaderComponent } from "../shared/header/app-header.component";
+import { IonContent} from "@ionic/angular/standalone";
+import { AppFooterComponent } from "../shared/footer/app-footer.component";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
   standalone: true,
+  imports: [ IonContent, AppHeaderComponent, AppFooterComponent],
 })
-export class AboutComponent  implements OnInit {
+export class AboutComponent {
 
-  constructor() { }
-
-  ngOnInit() {}
+  currentYear: number = new Date().getFullYear();
 
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { IonContent } from "@ionic/angular/standalone";
+import { AppHeaderComponent } from "../shared/header/app-header.component";
+import { AppFooterComponent } from "../shared/footer/app-footer.component";
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
   standalone: true,
+  imports: [AppHeaderComponent, AppFooterComponent, IonContent],
 })
 export class ContactComponent  implements OnInit {
 
