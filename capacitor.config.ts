54ba1d593c8ea638d7cf665e6274503accf2f1d6
AliciaAMT <46018401+AliciaAMT@users.noreply.github.com>
2025-06-15 23:56:40 -0500

    initial

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.accessiblewebmedia.app',
+  appName: 'mandalan-tales',
+  webDir: 'www'
+};
+
+export default config;
