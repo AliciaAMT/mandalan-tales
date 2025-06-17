6c8a914509f1edb338b10907c3b9fffe64e81a0a
AliciaAMT <46018401+AliciaAMT@users.noreply.github.com>
2025-06-17 10:46:29 -0500

    fighting dom

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { AppHeaderComponent } from "../shared/header/app-header.component";
-import { IonContent } from "@ionic/angular/standalone";
+import { IonContent, IonList, IonTitle, IonHeader, IonToolbar, IonItem, IonLabel, IonInput, IonButton, IonText, IonCardHeader, IonCardTitle, IonCard } from "@ionic/angular/standalone";
 import { AppFooterComponent } from "../shared/footer/app-footer.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
-  imports: [IonContent, AppHeaderComponent, AppFooterComponent],
+  imports: [IonCard, IonCardTitle, IonCardHeader,  IonButton, IonInput, IonLabel, IonItem, IonList, IonContent, AppHeaderComponent, AppFooterComponent, FormsModule],
 })
 export class LoginComponent {
+  email = '';
+  password = '';
 
+  login() {
+    console.log('Logging in:', this.email, this.password);
+  }
   currentYear: number = new Date().getFullYear();
 }
