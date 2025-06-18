3d4a69ff4870af311d0c658ddb3a69b9ff2e244d
AliciaAMT <46018401+AliciaAMT@users.noreply.github.com>
2025-06-18 07:25:54 -0500

    Refactor login component: remove unused imports and clean up HTML comments

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,17 @@
 
-import { AppHeaderComponent } from "../shared/header/app-header.component";
-
-import { AppFooterComponent } from "../shared/footer/app-footer.component";
 import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service'; // update path if needed
-import { Router } from '@angular/router';
-import { NgIf } from "@angular/common";
+import { Router, RouterModule } from '@angular/router';
 import { STANDALONE_IMPORTS } from '../shared/standalone-imports';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
-  imports: [AppHeaderComponent, AppFooterComponent, FormsModule, NgIf, STANDALONE_IMPORTS],
+  imports: [FormsModule, STANDALONE_IMPORTS, RouterModule],
 })
 export class LoginComponent {
   email = '';
